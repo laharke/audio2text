7d0f928ea81d4b8b6e55c7d6e82c718d18524aab
laharke <ronconimauro@yahoo.com.ar>
2023-05-19 10:47:09 -0300

    added spanish/english option + you can upload a .ogg file and it convertes it to wav

--- audio.py
+++ audio.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from tkinter import filedialog
 import speech_recognition as sr
+from pydub import AudioSegment
+import os
 
-def audio_to_text(audio_file):
+def audio_to_text(audio_file, language):
     recognizer = sr.Recognizer()
 
     # Load the audio file
@@ -10,10 +12,14 @@ def audio_to_text(audio_file):
         audio = recognizer.record(source)
 
     try:
-        # Use the default Google Web Speech API to convert audio to text
-        # recognize_google(audio, language="fr-FR")
-        text = recognizer.recognize_google(audio, language="es-AR")
-        print(text)
+        # Use the selected language for speech recognition
+        if language == "English":
+            text = recognizer.recognize_google(audio, language="en-US")
+        elif language == "Spanish":
+            text = recognizer.recognize_google(audio, language="es-AR")
+        else:
+            text = "Unsupported language"
+
         return text
     except sr.UnknownValueError:
         print("Speech recognition could not understand audio")
@@ -22,18 +28,29 @@ def audio_to_text(audio_file):
 
 def open_file():
     # Open a file dialog to select an audio file
-    file_path = filedialog.askopenfilename(filetypes=[("Audio Files", "*.wav")])
+    file_path = filedialog.askopenfilename(filetypes=[("Audio Files", "*.ogg")])
 
-    # Convert audio to text
     if file_path:
-        text = audio_to_text(file_path)
-        result_label.config(text="Text from audio: " + text)
+        # Convert .ogg to .wav
+        wav_file_path = file_path[:-4] + ".wav"
+        convert_ogg_to_wav(file_path, wav_file_path)
+
+        # Convert audio to text
+        language = language_var.get()
+        text = audio_to_text(wav_file_path, language)
+        result_label.config(text="Text from audio:\n" + text)
+
+        # Clean up temporary .wav file
+        os.remove(wav_file_path)
+
+def convert_ogg_to_wav(ogg_file, wav_file):
+    ogg_audio = AudioSegment.from_file(ogg_file, format="ogg")
+    ogg_audio.export(wav_file, format="wav")
 
 # Create the GUI window
 window = tk.Tk()
 window.title("Audio to Text")
 window.geometry("400x400")
-
 
 # Create a frame to hold the label and make it expand to fill the window
 frame = tk.Frame(window)
@@ -42,6 +59,12 @@ frame.pack(fill=tk.BOTH, expand=True)
 # Create a label to display the result
 result_label = tk.Label(frame, text="", wraplength=350, justify=tk.LEFT)
 result_label.pack(fill=tk.BOTH, padx=10, pady=10, expand=True)
+
+# Create a language selection dropdown menu
+language_var = tk.StringVar()
+language_var.set("Spanish")  # Default selection
+language_menu = tk.OptionMenu(window, language_var, "English", "Spanish")
+language_menu.pack(pady=10)
 
 # Create a button to open the file dialog
 button = tk.Button(window, text="Select Audio File", command=open_file)
